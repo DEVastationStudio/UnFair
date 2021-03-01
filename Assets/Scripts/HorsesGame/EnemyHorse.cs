f28a24417c838f73d77767411e13981ef3f3d5fa
SergioFQ <sergio.fq25@gmail.com>
2021-03-01 17:58:41 +0100

    Folders Organizatrions of horses game

---
+++ Assets/Scripts/HorsesGame/EnemyHorse.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHorse : MonoBehaviour
+{
+    private float mov;
+    private Vector3 newPos;
+    private Vector3 aux;
+    private bool move;
+
+    void Start()
+    {
+        move = false;
+    }
+
+
+    void Update()
+    {
+        mov = Random.Range(0.05f, 0.20f);
+        GetPos();
+    }
+
+    private void FixedUpdate()
+    {
+        float rand = Random.Range(0.0f,15.0f);
+        if (rand<12f)
+        {
+            return;
+        }
+            move = false;
+            transform.position = Vector3.MoveTowards(transform.position, newPos, 0.05f);
+
+    }
+
+    private void GetPos()
+    {
+        aux = (Vector3.forward * mov);
+        newPos = new Vector3(transform.position.x + aux.x, transform.position.y + aux.y, transform.position.z + aux.z);
+    }
+}
---
+++ Assets/Scripts/HorsesGame/PlayerHorse.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerHorse : MonoBehaviour
+{
+    [SerializeField] private PlayerInput input;
+    private float mov;
+    private Vector3 newPos;
+    private Vector3 aux;
+    private bool pulsado;
+
+    void Start()
+    {
+        pulsado = false;
+    }
+
+    private void OnSpaceAction(InputValue value)
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        mov = Random.Range(0.05f, 0.13f);
+        aux = (Vector3.forward * mov);
+        newPos = new Vector3(transform.position.x+aux.x, transform.position.y + aux.y, transform.position.z + aux.z);
+        pulsado = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (pulsado)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, newPos, 1);            
+            pulsado = false;
+        }
+    }
+
+}
