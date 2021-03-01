e5c2e2d9367fd24b6bca0af77f85b4b4bdbf0b55
SergioFQ <sergio.fq25@gmail.com>
2021-03-01 22:35:23 +0100

    Randomized key combination in horses game and detected it

--- Assets/Scripts/HorsesGame/PlayerHorse.cs
+++ Assets/Scripts/HorsesGame/PlayerHorse.cs
@@ -10,22 +10,121 @@ public class PlayerHorse : MonoBehaviour
     private Vector3 newPos;
     private Vector3 aux;
     private bool pulsado;
-
+    public string[] comb;
+    [SerializeField] private string[] availableKeys;
+    private int posComb;
+    private int correctSequence;
+    private bool combCreated;
     void Start()
     {
+        combCreated = false;
         pulsado = false;
+        GenerateCombination();
+        posComb = 0;
+        correctSequence = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            //Debug.Log(availableKeys[i]);
+        }
     }
 
+    private void GenerateCombination()
+    {
+        /*comb = new string[3];
+        Debug.Log(comb.Length);
+        comb[0] = "A";
+        comb[1] = "D";
+        comb[2] = "Space";*/
+        int keys = Random.Range(3,6);
+        comb = new string[keys];
+        string temp = "";
+        Debug.Log("Combinación a realizar");
+        for(int i = 0; i< comb.Length; i++)
+        {
+            temp = (string)availableKeys[Random.Range(0,6)];
+
+            //comprobar que la combinacion no use mas de 2 veces seguidas la misma tecla
+            if(i>2){
+                //for(int j = i-2; j< i; j++){
+                    if(temp.Equals(comb[i-2]) && temp.Equals(comb[i-1])){
+                        Debug.Log("Mucha repetición");
+                        string aux;
+                        do{
+                            aux = (string)availableKeys[Random.Range(0,6)];
+                        }
+                        while(!aux.Equals(temp));
+                        temp = aux;
+                    }
+                //}
+            }
+            //
+            comb[i] = temp;
+            Debug.Log(comb[i]);
+        }
+        
+        /*Debug.Log(comb[0]);
+        Debug.Log(comb[1]);
+        Debug.Log(comb[2]);*/
+        combCreated = true;
+    }
+
+    private void CombinationManagement(string keyPressed)
+    {
+        if(!combCreated){ return;}
+        if(posComb<comb.Length){
+            if(comb[posComb].Equals(keyPressed)){
+                correctSequence++;
+                if(correctSequence==comb.Length){
+                    Move();
+                }else{
+                    posComb ++;
+                }
+
+            }else{
+                correctSequence = 0;
+                posComb = 0;
+            }
+        }    
+        
+    }
     private void OnSpaceAction(InputValue value)
     {
-        Move();
+        CombinationManagement("Space");
     }
 
+    private void OnAAction(InputValue value)
+    {        
+        CombinationManagement("A");
+    }
+
+    private void OnDAction(InputValue value)
+    {
+        CombinationManagement("D");        
+    }
+
+    private void OnWAction(InputValue value)
+    {
+        CombinationManagement("W");        
+    }
+
+    private void OnSAction(InputValue value)
+    {
+        CombinationManagement("S");        
+    }
+    
+    private void OnEAction(InputValue value)
+    {
+        CombinationManagement("E");        
+    }
     private void Move()
     {
-        mov = Random.Range(0.05f, 0.13f);
+        correctSequence = 0;
+        posComb = 0;
+        Debug.Log("combinacion correcta");
+        mov = Random.Range(0.1f, 0.2f);
         aux = (Vector3.forward * mov);
         newPos = new Vector3(transform.position.x+aux.x, transform.position.y + aux.y, transform.position.z + aux.z);
+        transform.position = Vector3.MoveTowards(transform.position, newPos, 1);
         pulsado = true;
     }
 
@@ -38,7 +137,7 @@ public class PlayerHorse : MonoBehaviour
     {
         if (pulsado)
         {
-            transform.position = Vector3.MoveTowards(transform.position, newPos, 1);            
+            //transform.position = Vector3.MoveTowards(transform.position, newPos, 1);            
             pulsado = false;
         }
     }
