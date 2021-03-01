118b404d1a0bca2af2e43f8d27ca09ce3f4e42c9
plpgamedev <plpgamedev@gmail.com>
2021-03-01 21:14:57 +0100

    Shooting minigame update
    + You can shoot now with left Click
    + targets disapear when hit

---
+++ Assets/Scripts/TiroAlBlanco/MiraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiraScript : MonoBehaviour
+{
+    [SerializeField] private GameObject _mira;
+    [SerializeField] private GameObject _mousePoint;
+
+    void Update()
+    {
+        //Opcion 1
+        //Vector3 pos = (_mousePoint.transform.position - _mira.transform.position);
+        //_mira.transform.position += pos.normalized * pos.magnitude * Time.deltaTime * 2;
+
+        //Opcion 2
+    }
+}
