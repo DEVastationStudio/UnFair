e5e577d8a929bcde84dfa3c35633fbcad15311fd
plpgamedev <plpgamedev@gmail.com>
2021-03-01 17:56:23 +0100

    Folder organization for shooting minigame

---
+++ Assets/Scripts/TiroAlBlanco/pruebasMira.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class pruebasMira : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 viewPortPos = Camera.main.ScreenToViewportPoint(Mouse.current.position.ReadValue());
+        if (!(viewPortPos.x > 1 || viewPortPos.x < 0 || viewPortPos.y > 1 || viewPortPos.y < 0)) 
+        {
+            Ray r = Camera.main.ViewportPointToRay(viewPortPos);
+            Vector3 rPos = r.GetPoint(10);
+            transform.position = new Vector3(rPos.x, rPos.y, transform.position.z);
+        }
+    }
+}
