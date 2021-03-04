8546fc0a237b1a305766d4923669dca3c33f40b6
Zeus guy <Zeusguy@hotmail.es>
2021-03-04 17:10:31 +0100

    Implemented water shader and movement

--- Assets/Scripts/Ducks/FloatingObject.cs
+++ Assets/Scripts/Ducks/FloatingObject.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class FloatingObject : MonoBehaviour
 {
     public Rigidbody rigidBody;
+
+    private float waterHeight;
+
+    void Start() {
+        waterHeight = 0;
+    }    
     void FixedUpdate()
     {
-        if (transform.position.y < 0)
+        waterHeight = (Mathf.Cos(transform.position.y) + Mathf.Cos(transform.position.x + (5 * Time.timeSinceLevelLoad)) + Mathf.Cos(transform.position.z + (5 * Time.timeSinceLevelLoad)))*0.1f;
+
+        if (transform.position.y < waterHeight)
         {
-            rigidBody.AddForce(new Vector3(0, Mathf.Abs(Physics.gravity.y) * (Mathf.Clamp(-transform.position.y, 0, 1) * 3), 0), ForceMode.Acceleration);
+            //Floating force
+            rigidBody.AddForce(new Vector3(0, Mathf.Abs(Physics.gravity.y) * (Mathf.Clamp(waterHeight-transform.position.y, 0, 1) * 3), 0), ForceMode.Acceleration);
+            
+            //Spin force
+            Vector3 cross = Vector3.Cross(transform.position, Vector3.up);
+            cross.y = 0;
+            Vector3 pos = -transform.position;
+            pos.y = 0;
+            rigidBody.AddForce(cross, ForceMode.Force);
+            rigidBody.AddForce(pos, ForceMode.Force);
+            Debug.DrawRay(transform.position, Vector3.Cross(transform.position, Vector3.up));
         }
     }
 }
