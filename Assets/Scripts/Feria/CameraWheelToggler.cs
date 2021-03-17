26d3eba7bfb6b6507794ab0acff311a7cec33676
Zeus guy <Zeusguy@hotmail.es>
2021-03-17 19:56:09 +0100

    Update fair camera
    - A different camera is now used when near the ferris wheel
    
    - The main camera is now limited to the fair itself

---
+++ Assets/Scripts/Feria/CameraWheelToggler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWheelToggler : MonoBehaviour
+{
+    public GameObject vc1, vc2;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player")
+        {
+            vc1.SetActive(false);
+            vc2.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.tag == "Player")
+        {
+            vc1.SetActive(true);
+            vc2.SetActive(false);
+        }
+    }
+}
