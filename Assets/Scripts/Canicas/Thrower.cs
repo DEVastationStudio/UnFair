18597ffa6fafd1128abd2cf2a135dc11c7313bf4
SergioFQ <sergio.fq25@gmail.com>
2021-03-22 19:29:35 +0100

    Added marbles scene
    Created marble scene for the new minigame. Also  created a small scene with some objects and a ball thrower. Added discontinuos line renderer in order to simulate the ball trajectory.

--- Assets/Scripts/Canicas/Thrower.cs
+++ Assets/Scripts/Canicas/Thrower.cs
@@ -1,18 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Thrower : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private PlayerInput input;
+    [SerializeField] private GameObject ballPref;
+    [SerializeField] private GameObject firePoint;
+    [SerializeField] private float power;
+    [SerializeField] private float rotationSpeed;
+
+    private Vector3 currentPos;
+    Quaternion currentRot;
+    private Trajectory trajectory;
+    private int test = 0;
+
+
     void Start()
     {
-        
+        trajectory = FindObjectOfType<Trajectory>();
+        currentPos = transform.position;
+        currentRot = transform.rotation;
+        CreatePrediction();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //poner como rotar y moverse para poder cambiar hacia donde va la pelota y eso
+        if (currentRot != transform.rotation)
+        {
+            CreatePrediction();
+        }
+
+        if (currentPos != transform.position)
+        {
+            CreatePrediction();
+        }
+
+        currentRot = transform.rotation;
+        //pulsar para disparar
+
+    }
+
+    public Vector3 CalculateForce()
+    {
+        return transform.forward * power;
+    }
+
+    void ThrowBall()
+    {
+        GameObject ball = Instantiate(ballPref, firePoint.transform.position, Quaternion.identity);
+        ball.GetComponent<Rigidbody>().AddForce(CalculateForce(), ForceMode.Impulse);
+    }
+
+    void CreatePrediction()
+    {
+        trajectory.PathCreation(ballPref, firePoint.transform.position, CalculateForce());
+    }
+
+
+    private void OnSpaceAction(InputValue value)
+    {
+        ThrowBall();
+    }
+
+    private void OnAAction(InputValue value)
+    {
+    }
+
+    private void OnDAction(InputValue value)
+    {
     }
 }
