c295314183cd39a80eb0191588f378d813a49c0b
Zeus guy <Zeusguy@hotmail.es>
2021-04-05 19:23:14 +0200

    Add NPC conversation before entering Ducks minigame

---
+++ Assets/Scripts/Feria/NPCs/SpriteTurner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteTurner : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private SpriteRenderer _sprite;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camPos = _mainCamera.transform.position;
+        camPos.y = _sprite.transform.position.y;
+
+        _sprite.transform.forward = camPos - _sprite.transform.position;
+
+        _sprite.flipX = true;
+    }
+}
