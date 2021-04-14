7925912e5654095399696f280669cf4182ab0b0d
plpgamedev <plpgamedev@gmail.com>
2021-04-14 19:58:06 +0200

    Add Settings prefab & Settings implementation for pause menu

---
+++ Assets/Scripts/Feria/Interfaz/AjustesManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AjustesManager : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private List<Button> _botones;
+    [SerializeField] private List<GameObject> _pestañas;
+    [SerializeField] private List<Slider> _primerosAjustes;
+
+    private int _activeId = 0;
+
+    #endregion Variables
+
+    #region Metodos
+
+    public void ChangePestaña(int n) 
+    {
+        _pestañas[_activeId].SetActive(false);
+        _activeId = n;
+        _pestañas[_activeId].SetActive(true);
+        if (n < _primerosAjustes.Count)
+        {
+            for (int i = 0; i < _pestañas.Count; i++)
+            {
+                Navigation nav = _botones[i].navigation;
+                nav.selectOnDown = _primerosAjustes[n];
+                _botones[i].navigation = nav;
+            }
+        }
+    }
+
+    #endregion Metodos
+}
