2f8f5a7c1211af8a15215f45b39755c16bd9181b
plpgamedev <plpgamedev@gmail.com>
2021-07-03 13:28:03 +0200

    feat: add support for multiple shot sprites which are selected randomly & fix wall colliders

--- Assets/Scripts/TiroAlBlanco/Minigame/ShotSprite.cs
+++ Assets/Scripts/TiroAlBlanco/Minigame/ShotSprite.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class ShotSprite : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer _sprite;
+    [SerializeField] private SpriteRenderer[] _sprite;
+    private int _shotType;
 
     void Start()
     {
+        _shotType = Random.Range(0, _sprite.Length-1);
+        _sprite[_shotType].gameObject.SetActive(true);
         StartCoroutine(Disapear());
     }
 
     IEnumerator Disapear() 
     {
         yield return new WaitForSeconds(1);
-        Color aux = _sprite.color;
+        Color aux = _sprite[_shotType].color;
         float count = 0;
         while (count < 1) 
         {
             yield return new WaitForSeconds(0.05f);
             aux.a -= 0.05f;
             count += 0.05f;
-            _sprite.color = aux;
+            _sprite[_shotType].color = aux;
         }
         Destroy(gameObject);
     }
