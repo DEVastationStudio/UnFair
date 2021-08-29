8637edb10476eaa64d922278ebdd2d1ab5e1d32b
Zeus-guy <Zeusguy@hotmail.es>
2021-08-29 17:13:06 +0200

    feat: Implement haunted house route

---
+++ Assets/Scripts/Debug/DebugManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugManager : MonoBehaviour
+{
+    public NpcSpriteManager npcSpriteManager;
+    public int Progression = 0;
+    public void SetProgression()
+    {
+        print("Set Progression to " + Progression);
+        PlayerPrefs.SetInt("Progression", Progression);
+        if (Application.isPlaying) npcSpriteManager.UpdateSprites();
+    }
+    public int ProgAlt1 = 0;
+    public void SetProgAlt1()
+    {
+        print("Set ProgAlt1 to " + ProgAlt1);
+        PlayerPrefs.SetInt("ProgAlt1", ProgAlt1);
+        if (Application.isPlaying) npcSpriteManager.UpdateSprites();
+    }
+    public int ProgAlt2= 0;
+    public void SetProgAlt2()
+    {
+        print("Set ProgAlt2 to " + ProgAlt2);
+        PlayerPrefs.SetInt("ProgAlt2", ProgAlt2);
+        if (Application.isPlaying) npcSpriteManager.UpdateSprites();
+    }
+    //Set Stars
+}
---
+++ Assets/Scripts/Feria/Alternate Routes/HauntedHouseManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HauntedHouseManager : MonoBehaviour
+{
+    public ConversationHelper cutscene;
+    public Image fade;
+    void Start()
+    {
+        FadeController.FinishLoad();
+        cutscene.StartConversation();
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(ActivateFade());
+    }
+
+    public void FadeIn()
+    {
+        StartCoroutine(DeactivateFade());
+    }
+
+    private IEnumerator ActivateFade()
+    {
+        fade.enabled = true;
+        float time = 0;
+        while (time < 2)
+        {
+            fade.color = Color.Lerp(fade.color, Color.black, time/2);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        fade.color = Color.black;
+    }
+    private IEnumerator DeactivateFade()
+    {
+        float time = 0;
+        while (time < 2)
+        {
+            fade.color = Color.Lerp(fade.color, Color.clear, time/2);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        fade.color = Color.clear;
+        fade.enabled = false;
+    }
+}
